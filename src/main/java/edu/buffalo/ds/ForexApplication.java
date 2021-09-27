47b9f52ad904dd40a823d614c40500941d3e877f
aditya <adityano@buffalo.edu>
2021-09-26 21:10:52 -0400

    Enable cors

--- src/main/java/edu/buffalo/ds/ForexApplication.java
+++ src/main/java/edu/buffalo/ds/ForexApplication.java
@@ -10,6 +10,11 @@ import edu.buffalo.ds.service.UserInfoService;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class ForexApplication extends Application<ApplicationConfiguration> {
     public static void main(String[] args) throws Exception {
@@ -30,6 +35,18 @@ public class ForexApplication extends Application<ApplicationConfiguration> {
         final ForexResource resource = new ForexResource(
                 new UserInfoService(userInfoDAO)
         );
+
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
         environment.jersey().register(resource);
         environment.lifecycle().manage(mongoDBManaged);
     }
